655805751f66a29b4957662a9957bc27a36e0914
Mikhail Shipov <mshipov@yandex.ru>
2022-10-27 10:48:00 +0300

    Revert "chore: filter private pkgs from published list"
    This reverts commit 6a887a84620cef5242c845bd6880e6fcee7c285e.

--- website/download-pvm-packages.mjs
+++ website/download-pvm-packages.mjs
@@ -1,5 +1,6 @@
 import fs from "fs";
 import path from "path";
+import fetch from "node-fetch";
 import { pkgset } from "@pvm/pkgset";
 import drainItems from "@pvm/core/lib/iter/drain-items.js";
 import {fileURLToPath} from 'url';
@@ -8,9 +9,10 @@ const __filename = fileURLToPath(import.meta.url);
 
 const pvmPackages = (await drainItems.default(pkgset('all', {
   cwd: path.join(path.dirname(__filename), '..')
-}))).filter(pkg => !pkg.meta.private).map(pkg => ({
+}))).map(pkg => ({
   name: pkg.name,
   version: pkg.version,
+  tgz: `${pkg.shortName}-${pkg.version}.tgz`,
 }))
 
 console.log('@pvm/* packages', JSON.stringify(pvmPackages, null, 2))
@@ -20,3 +22,12 @@ if (!fs.existsSync(artifactsPath)) {
   fs.mkdirSync(artifactsPath)
 }
 fs.writeFileSync(path.join(artifactsPath, 'packages.json'), JSON.stringify(pvmPackages))
+pvmPackages.forEach(async ({ name, tgz }) => {
+  const pkgTarPath = path.join(artifactsPath, tgz)
+  const pkgTarUrl = `https://registry.npmjs.org/${name}/-/${tgz}`
+
+  console.log(`Downloading ${pkgTarUrl} to ${pkgTarPath}`)
+  const pkgData = await (await fetch(pkgTarUrl)).arrayBuffer()
+
+  fs.writeFileSync(pkgTarPath, Buffer.from(pkgData))
+})
