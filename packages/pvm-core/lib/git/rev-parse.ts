33bb99fe8b33263dd9e44d76cd840bdbadba49a0
Mikhail Shipov <mshipov@yandex.ru>
2022-11-10 12:37:14 +0300

    fix(@pvm/core): revParse optimization

--- packages/pvm-core/lib/git/rev-parse.ts
+++ packages/pvm-core/lib/git/rev-parse.ts
@@ -1,10 +1,28 @@
 import { wdShell } from '../shell'
+import path from 'path'
+import fs from 'fs'
 
 const revListCache = new Map<string, string>()
 
 export default function revParse(ref: string, cwd: string): string {
+  // converting HEAD to commit sha without starting separate process which might be slow and up to 50ms
+  // https://git-scm.com/docs/gitglossary#Documentation/gitglossary.txt-aiddefsymrefasymref
+  // https://git-scm.com/docs/gitrepository-layout#Documentation/gitrepository-layout.txt-HEAD
+  const headPath = path.join(cwd, '.git', 'HEAD')
+  if (ref === 'HEAD' && fs.existsSync(headPath)) {
+    const headContent = fs.readFileSync(headPath, 'utf-8').trim()
+    if (headContent) {
+      // symref
+      if (headContent.startsWith('ref: ')) {
+        return fs.readFileSync(path.join(cwd, '.git', headContent.split('ref: ')[1]), 'utf-8')
+      // plain commit sha
+      } else {
+        return headContent
+      }
+    }
+  }
+
   const cacheKey = `${cwd}_${ref}`
-  // HEAD не кешируем
   const cachedResult = revListCache.get(cacheKey)
   if (ref.startsWith('HEAD') || !cachedResult) {
     const result = wdShell(cwd, `git rev-list -1 ${ref}`)
