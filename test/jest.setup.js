980fec3266e207dbab10b90dc32b28bb70c1dc6b
Mikhail Shipov <mshipov@yandex.ru>
2022-11-11 16:34:09 +0300

    fix: add total perf metrics

--- test/jest.setup.js
+++ test/jest.setup.js
@@ -11,10 +11,8 @@ global.writeRepo = writeRepo
 
 Object.assign(process.env, {
   PVM_CONFIG_SEARCH_FROM: __dirname,
-  __PVM_ENABLE_INVALIDATE_RELEASES_BUT_IT_MAY_RESULT_IN_A_DENIAL_OF_SERVICE: 'true',
   PVM_CONFIG_JIRA__URL: 'https://jira.example.com',
   PVM_TEST_DATE_NOW: '2018-11-27T12:00:00.000Z',
-  // SLACK_WEBHOOK: `http://localhost:${process.env.GITLAB_HTTP_PORT}/slack-hook/`,
   CI_PROJECT_NAMESPACE: 'pfp',
   CI_PROJECT_NAME: 'test-p',
   CI_PROJECT_ID: 111,
